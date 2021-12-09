6e208236f6a777bf0c0a386336d2619662e13308
Eric Labaci <eric.labaci@gmail.com>
2021-12-09 09:23:09 -0300

    Creates formula

---
+++ Formula/swift-double-generator.rb
@@ -0,0 +1,15 @@
+class SwiftDoubleGenerator < Formula
+  desc "Double generator to protocols"
+  homepage "https://github.com/ericlabaci/swift-double-generator"
+  url "https://github.com/ericlabaci/swift-double-generator.git", tag: "0.0.2"
+  license ""
+  head "https://github.com/ericlabaci/swift-double-generator.git"
+
+  def install
+      system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+      system "#{bin}/double-generator" "generate-double Test.swift"
+  end
+end
